8f8821346ebdd055f8fe5d55ed3fe5971af59673
sudo-Manan <called.manan.i.am@gmail.com>
2026-05-30 01:22:56 +0530

    add: binary to gray converter
    dataflow model based binary to gray logic and testbench

---
+++ combinational_logic/dataflow_model/code_converters/bin2gray.sv
@@ -0,0 +1,13 @@
+module bin2gray #(parameter WIDTH = 4) (gray, bin);
+    input logic [WIDTH-1:0] bin;
+    output logic [WIDTH-1:0] gray;
+
+    assign gray[WIDTH-1] = bin[WIDTH-1]; // MSB remains the same
+    genvar i;
+    generate 
+        for (i = 0; i < WIDTH-1; i = i + 1) begin : gen_gray
+            assign gray[i] = bin[i] ^ bin[i+1];
+        end
+    endgenerate
+
+endmodule
---
+++ combinational_logic/dataflow_model/code_converters/bin2gray_tb.sv
@@ -0,0 +1,24 @@
+module bin2gray_tb;
+    parameter WIDTH = 4;
+    logic [WIDTH-1:0] bin_in;
+    logic [WIDTH-1:0] gray_out;
+
+    bin2gray #(WIDTH) dut1 (.gray(gray_out), .bin(bin_in));
+
+    //use with icarus verilog and terosHDL on vs code to generate output vvp and waveform
+    // initial begin
+    //     $dumpfile("bin2gray_tb.vcd");
+    //     $dumpvars(0, bin2gray_tb);
+    // end
+
+    initial begin
+        // Test all possible binary inputs
+        $monitor("Binary Input: %b | Gray Output: %b", bin_in, gray_out);
+        #10;
+        for (int i = 0; i < (1 << WIDTH); i=i+1) begin
+            bin_in = i;
+            #10; // Wait for the output to stabilize
+        end
+        $finish;
+    end
+endmodule
